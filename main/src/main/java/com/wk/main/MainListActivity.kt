e5754b94d2a606c14db6621de088bdd92eeaafb1
wk1995 <1226426603@qq.com>
2018-10-08 20:30:15 +0800

    各个组件集成后的入口

---
+++ main/src/main/java/com/wk/main/MainListActivity.kt
@@ -0,0 +1,58 @@
+package com.wk.main
+
+import android.os.Bundle
+import com.alibaba.android.arouter.launcher.ARouter
+import com.wk.common.base.BaseMainListActivity
+import com.wk.common.constant.BundleKey
+import com.wk.common.constant.RouterPath
+
+class MainListActivity : BaseMainListActivity() {
+    private val activityList by lazy {
+        val activityList=ArrayList<String>()
+        activityList.add("hotfix")
+        activityList.add("list")
+        activityList.add("picture")
+        activityList.add("view")
+        activityList.add("rxJava")
+        activityList.add("okHttp")
+        activityList.add("retrofit")
+        activityList
+    }
+    override fun getRecyclerItemList()=activityList
+
+    override fun onRecyclerItemClick(bundle: Bundle?,vararg objects: Any?) {
+        val itemText=bundle?.getString(BundleKey.ItemText)
+        when(itemText){
+            activityList[0]->ARouter
+                    .getInstance()
+                    .build(RouterPath.HotfixActivityMainActivity)
+                    .navigation()
+            activityList[1]->ARouter
+                    .getInstance()
+                    .build(RouterPath.ListActivityMainActivity)
+                    .navigation()
+            activityList[2]->ARouter
+                    .getInstance()
+                    .build(RouterPath.PicMainActivity)
+                    .navigation()
+            activityList[3]->ARouter
+                    .getInstance()
+                    .build(RouterPath.ViewMainActivity)
+                    .navigation()
+            activityList[4]->ARouter
+                    .getInstance()
+                    .build(RouterPath.RxJavaMainActivity)
+                    .navigation()
+            activityList[5]->ARouter
+                    .getInstance()
+                    .build(RouterPath.OkHttpMainActivity)
+                    .navigation()
+            activityList[6]->ARouter
+                    .getInstance()
+                    .build(RouterPath.RetrofitMainActivity)
+                    .navigation()
+        }
+
+    }
+
+}
---
+++ main/src/main/java/com/wk/okhttp/debug/MainApplication.kt
@@ -0,0 +1,15 @@
+package com.wk.okhttp.debug
+
+import com.wk.common.BaseApplication
+
+/**
+ * <pre>
+ *      author : wk
+ *      e-mail : 1226426603@qq.com
+ *      time   : 2018/08/20
+ *      desc   :
+ *      GitHub : https://github.com/wk1995
+ *      CSDN   : http://blog.csdn.net/qq_33882671
+ * </pre>
+ */
+class MainApplication:BaseApplication()
