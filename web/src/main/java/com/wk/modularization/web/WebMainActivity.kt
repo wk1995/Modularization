9573cdea82c081594af4a007e7ca77b144eab2b1
wk1995 <1226426603@qq.com>
2019-01-15 21:13:20 +0800

    提交web控件的使用例子

--- web/src/main/java/com/wk/modularization/web/WebMainActivity.kt
+++ web/src/main/java/com/wk/modularization/web/WebMainActivity.kt
@@ -1,17 +1,24 @@
 package com.wk.modularization.web
 
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebView
 import com.wk.common.base.BaseActivity
 import com.wk.view.R
 import kotlinx.android.synthetic.main.web_activity_main.*
 
-class WebMainActivity : BaseActivity() {
+class WebMainActivity : BaseActivity(), View.OnClickListener {
 
+    private var webView: WebView? = null
 
-    override fun getLayoutResource()=R.layout.web_activity_main
+    override fun getLayoutResource() =-1
 
     override fun initView() {
         super.initView()
-        webView.loadUrl("https://github.com/JakeWharton/butterknife")
+        webView = WebView(this.applicationContext)
+        setContentView(webView)
+        webView?.loadUrl("https://www.baidu.com/")
+
     }
 
     override fun initListener() {
@@ -22,4 +29,24 @@ class WebMainActivity : BaseActivity() {
         super.initParam()
     }
 
+    override fun onClick(v: View?) {
+        when (v) {
+            btSearch -> {
+                val webSite = etWebsite.text.toString().trim()
+                webView?.loadUrl(webSite)
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        if (webView != null) {
+            webView?.loadDataWithBaseURL(null, "", "text/html", "utf-8", null)
+            webView?.clearHistory()
+            (webView?.parent as? ViewGroup)?.removeAllViews()
+            webView?.destroy()
+            webView = null
+        }
+        super.onDestroy()
+
+    }
 }
